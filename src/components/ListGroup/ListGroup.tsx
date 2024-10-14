2ca77a94d53f275f9b142cd78131d9f9cf0b6a48
danielifc <danielifc@gmail.com>
2024-10-14 10:15:29 -0600

    implementation of css-in-js

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,21 @@
 import { useState } from "react";
-import styles from './ListGroup.module.css';
+import styles from './ListGroup.css';
+import styled from 'styled-components';
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+//aqui define que ListItemProps representa la forma de las propiedades del componente ListItem
+const ListItem = styled.li<ListItemProps>`
+  padding: 5 px 0;
+  background: ${ props => props.active ? 'blue' : 'none' }
+`;
 
 // {items: [], heading: string}
 //Aqui define los parametros que se enviaran a este componente!!
@@ -14,7 +30,7 @@ interface Props {
 //Hizo el destructuring de Props aqui:
 function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   //Event handler
   //   const handleClick = (event: MouseEvent) => console.log(event);
@@ -23,14 +39,10 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
-      <ul className={[styles.listGroup, styles.container].join(' ')}  >
+      <List>
         {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem
+            active={index === selectedIndex}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -38,9 +50,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
