6eff91c0c17026b37e852264802df7f5d9068cd5
danielifc <danielifc@gmail.com>
2024-10-15 21:03:59 -0600

    Adding icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
-import './App.css';
+import { BsFillCalendarFill } from "react-icons/bs";
+import "./App.css";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -13,14 +14,16 @@ function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
-    
+    <BsFillCalendarFill color="red" size="60" />
+
+    // <div>
+    //   <ListGroup
+    //     items={items}
+    //     heading="Cities"
+    //     onSelectItem={handleSelectItem}
+    //   />
+    // </div>
+
     // <div>
     //   {alertVisible && (
     //     <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
