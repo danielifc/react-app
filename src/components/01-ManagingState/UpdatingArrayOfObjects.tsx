607cdfcabf1ef9d62daa31c914b77d3fa994f7a2
danielifc <danielifc@gmail.com>
2024-10-20 20:47:37 -0600

    Simplifying update logic with immer

--- src/components/01-ManagingState/UpdatingArrayOfObjects.tsx
+++ src/components/01-ManagingState/UpdatingArrayOfObjects.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import produce from "immer";
 
 const UpdatingArrayOfObjects = () => {
   const [bugs, setBugs] = useState([
@@ -7,7 +8,14 @@ const UpdatingArrayOfObjects = () => {
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   // This will run every time 'tags' changes
@@ -17,6 +25,11 @@ const UpdatingArrayOfObjects = () => {
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "No fixed"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
